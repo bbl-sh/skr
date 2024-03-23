9610ea470b7fc2fe9c75c0ca42f2d42929da8977
bbl-sh <bhushanbharat2906@gmail.com>
2024-03-24 00:54:54 +0530

    second commit 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
 
 <Navbar /> 
 <main class="container mx-auto px-4 py-10">
-    <h2 class="text-3xl font-bold mb-8">Getting Started</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         <Card 
         title="My Card" 
